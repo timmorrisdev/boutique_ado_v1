48a3effaa785bfe38fb81b405dfe8c81abb71aa8
timmorrisdev <timmorris_16@hotmail.com>
2021-12-14 17:56:31 +0000

    Add basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -16,7 +16,7 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-    
+
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
@@ -26,14 +26,16 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51K6c7fDWdngorSBDqnAF6e1bx9BZrh8n0KpB4GZaUx2CIaomaZ0CsrQaPoyzNrK2yC25tiwCyhjEpW3Q3t6Nhdw200Ww4ShDCP',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
